f2b77edeba27485faa39498e786c60c8a2edcca0
Ikariius <116696270+Ikariius@users.noreply.github.com>
2023-05-16 14:24:10 +0700

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from task1 import * 
-from task2 import *
-from scheduler import *
+print("Hello LAB2")
 import time
-
+from scheduler import *
+from task1 import *
+from task2 import *
 scheduler = Scheduler()
 scheduler.SCH_Init()
 
-task1 =Task1()
-task2 =Task2()
+task1 = Task1()
+task2 = Task2()
 
-scheduler.SCH_Add_Task(task1.Task1_Run,1000,2000)
-scheduler.SCH_Add_Task(task2.Task2_Run,2000,4000)
+scheduler.SCH_Add_Task(task1.Task1_Run, 1000,2000)
+scheduler.SCH_Add_Task(task2.Task2_Run, 2000,4000)
 
 while True:
     scheduler.SCH_Update()
